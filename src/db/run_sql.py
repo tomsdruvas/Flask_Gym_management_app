eb53e48b12a02b5b5143eaf7bfd6577b31e0b51b
Toms Druvaskalns <tomsdruvas@gmail.com>
2022-01-17 22:27:22 +0000

    adding new db

--- src/db/run_sql.py
+++ src/db/run_sql.py
@@ -5,7 +5,8 @@ def run_sql(sql, values = None):
     results = []
     conn = None
     try:
-        conn = psycopg2.connect("dbname='gym_manager'")
+        # gym_manager
+        conn = psycopg2.connect("dbname='postgresql-animate-23710'")
         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cur.execute(sql, values)
         conn.commit()
